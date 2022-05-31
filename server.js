2951fcc8e267c862b60fc1209de021a551b87fee
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-05-30 21:38:23 -0300

    Salvando dados na database

--- server.js
+++ server.js
@@ -2,19 +2,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 var jsonParser = bodyParser.json();
 
-app.use(cors());
+var password = process.env.MONGO_PASSWORD;
 
-app.listen(5000, function () {
-  console.log("listening on 5000");
-});
+const uri = `mongodb+srv://quezze:${password}@cluster0.5wuhh.mongodb.net/?retryWrites=true&w=majority`;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+MongoClient.connect(uri, { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db("quezze");
+    const quizzCollection = db.collection("quizz");
 
-app.post("/quezze", jsonParser, (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-});
+    app.use(cors());
+
+    app.listen(5000, function () {
+      console.log("listening on 5000");
+    });
+
+    app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
+
+    app.post("/quezze", jsonParser, (req, res) => {
+      quizzCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => console.error(error));
+
+      res.send(req.body);
+    });
+  })
+  .catch((error) => console.error(error));
